95ae58d417e252e81a62e18aa68c06bc123b9af3
PetrIvChe <cherniavskiipetr@gmail.com>
2022-04-13 22:48:22 +0300

    The test Checkbox not checked is pass

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,8 +1,5 @@
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -52,40 +50,28 @@ public class CardOrderTest {
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79874574582");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
-        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldSendFormWhenHyphenInName() {
-        //driver.get("http://localhost:9999");
-
-//        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-//        textFields.get(0).sendKeys("Смирнова-Петрова Анна-Мария");
-//        textFields.get(1).sendKeys("+99999999999");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Смирнова-Петрова Анна-Мария");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79874574582");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
-        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldSendFormWhenNameIsEmpty() {
-        //driver.get("http://localhost:9999");
-
-//        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-//        textFields.get(0).sendKeys("");
-//        textFields.get(1).sendKeys("+99999999999");
-
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79874574582");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
-        //  String actual = driver.findElement(By.className("input__sub")).getText();
         String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub ")).getText();
         String expected = "Поле обязательно для заполнения";
         assertEquals(expected, actual);
@@ -93,12 +79,6 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendAlertMessageWhenNameOnEnglish() {
-        //driver.get("http://localhost:9999");
-
-//        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-//        textFields.get(0).sendKeys("Smith James");
-//        textFields.get(1).sendKeys("+00000000000");
-
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Smith James");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79874574582");
         driver.findElement(By.className("checkbox__box")).click();
@@ -110,10 +90,6 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendAlertMessageWhenPhoneNumberIsEmpty() {
-        //driver.get("http://localhost:9999");
-//        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-//        textFields.get(0).sendKeys("Иванов Сергей");
-//        textFields.get(1).sendKeys("");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Сергей");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
         driver.findElement(By.className("checkbox__box")).click();
@@ -125,11 +101,6 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendAlertMessageWhenPhoneNumberIsMore12Symbols() {
-        //driver.get("http://localhost:9999");
-//        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-//        textFields.get(0).sendKeys("Иванов Сергей");
-//        textFields.get(1).sendKeys("+373777156325");
-
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Сергей");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+373777156325");
         driver.findElement(By.className("checkbox__box")).click();
@@ -141,7 +112,6 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendAlertMessageWhenPhoneNumberWithoutPlus() {
-        //driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Сергей");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("373777156325");
         driver.findElement(By.className("checkbox__box")).click();
@@ -153,7 +123,6 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendAlertMessageWhenPhoneNumberWithPlusInMiddle() {
-        //driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Сергей");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("3737+77156325");
         driver.findElement(By.className("checkbox__box")).click();
@@ -165,15 +134,12 @@ public class CardOrderTest {
 
     @Test
     public void shouldSendAlertFormWhenNonCheckOnAgreementCheckbox() {
-        //driver.get("http://localhost:9999");
-        List<WebElement> textFields = driver.findElements(By.className("input__control"));
-        textFields.get(0).sendKeys("Иоган Кристиан");
-        textFields.get(1).sendKeys("+79874574582");
-        driver.findElement(By.className("checkbox__box")).isSelected();
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иоган Кристиан");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79874574582");
         driver.findElement(By.tagName("button")).click();
-        String actual = driver.getCurrentUrl();
-        String expected = "http://localhost:9999/";
-        assertEquals(expected, actual);
+        boolean actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__box")).isSelected();
+        assertTrue(true);
     }
 
 
